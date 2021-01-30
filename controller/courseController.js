11dacbdc053b5e327139c362babf7230c98f67fe
Yogesh-10 <yogiyogesh100800@gmail.com>
2021-01-30 13:18:18 +0530

    Advanced querying- sort,limit,pagination,select

--- controller/courseController.js
+++ controller/courseController.js
@@ -7,10 +7,72 @@ const Course = require('../models/courseModel')
 // @route GET/api/v1/courses
 // @access public
 const getAllCourses = asyncHandler(async (req, res, next) => {
-	const courses = await Course.find({})
+	let query
+
+	// create a copy of req.query
+	const reqQuery = { ...req.query }
+
+	// Fields to exclude
+	const removeFields = ['select', 'sort', 'page', 'limit']
+
+	// Loop and removeFields and delete from reqQuery
+	removeFields.forEach((param) => delete reqQuery[param])
+
+	// ceate a query string
+	let querystr = JSON.stringify(reqQuery)
+
+	// create operators($lte, $gte, etc...)
+	querystr = querystr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`)
+
+	// Finding resource
+	query = Course.find(JSON.parse(querystr))
+
+	// select fields
+	if (req.query.select) {
+		const fields = req.query.select.split(',').join(' ')
+		query = query.select(fields)
+	}
+
+	// sorting
+	if (req.query.sort) {
+		const sortBy = req.query.sort.split(',').join(' ')
+		query = query.sort(sortBy)
+	} else {
+		query = query.sort('-createdAt') //descending createdAt
+	}
+
+	// pagination-limit
+	const page = parseInt(req.query.page, 10) || 1
+	const limit = parseInt(req.query.limit, 10) || 1
+	const startIndex = (page - 1) * limit
+	const endIndex = page * limit
+	const total = await Course.countDocuments()
+
+	query = query.skip(startIndex).limit(limit)
+
+	// query Execution
+	const courses = await query
+
+	// pagination result
+	const pagination = {}
+	if (endIndex < total) {
+		pagination.next = {
+			page: page + 1,
+			limit,
+		}
+	}
+
+	if (startIndex > 0) {
+		pagination.prev = {
+			page: page - 1,
+			limit,
+		}
+	}
+
 	res.status(200).json({
 		success: true,
 		count: courses.length,
+		pagination,
 		data: courses,
 	})
 })
